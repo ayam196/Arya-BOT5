2804baf341204e9b9a420ff6b8871968d2170515
ReyAnckerman <77916501+ayam196@users.noreply.github.com>
2022-07-23 16:03:31 +0700

    donasi.js

--- plugins/donasi.js
+++ plugins/donasi.js
@@ -1,7 +1,7 @@
 let handler = async m => m.reply(`
 ╭─「 Donasi • Pulsa 」
-│ • Indosat Ooredoo [081515860089]
-│ • Telkomsel [081334177340]
+│ • Indosat Ooredoo [085892910131]
+│ • Saweria [https://saweria.co/Onlyfanny]
 ╰────
 `.trim()) // Tambah sendiri kalo mau
 handler.help = ['donasi']
